6de7b9f7d3401eebc3dd04b5531072c0608465f0
unknown <mjwolff@umass.edu>
2014-12-29 14:24:48 -0500

    index php

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+$sername = "ftp.personalizedcancercareinstitute.com"
+$usename = "matt@personalizedcancercareinstitute.com"
+$pass = "einszweithreefour"
+$dbname = "Ruben_fmhApp"
+
+$conn = new mysqli($sername, $usename, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "Select * from questions where setid = 1";
+$result = $conn->query($sql);
+
+if($result->num_rows > 0) {
+	echo '
+
+	<form action="action_page.php" method="POST">
+	<fieldset>
+	<legend>Personal information:</legend>
+	First name:<br>
+	<input type="text" name="firstname" value="Mickey">
+	<br>
+	Last name:<br>
+	<input type="text" name="lastname" value="Mouse">
+	<br><br>
+	</fieldset>
+	'
+
+	echo'
+	<input type="submit" value="Submit">
+	</form>
+
+	'
+}
+
+?>
