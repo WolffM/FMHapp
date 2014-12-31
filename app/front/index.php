8cb3815dbc77670c88dac7d203656c2536f7c3fa
unknown <mjwolff@umass.edu>
2014-12-31 18:28:15 -0500

    bootstrap

--- app/front/index.php
+++ app/front/index.php
@@ -30,20 +30,20 @@ $sql = "SELECT * FROM questions WHERE questionset = 1;";
 	<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	';
 
-	echo '
+	echo "
 
-	<form action="results.php" method="POST">
-	<fieldset>
-	<legend>Personal information:</legend>
-	Identifier:<br>
-	<input type="text" name ="name" value="No Name" required value="1">
+	<form action='results.php' method='POST'>
+	<div class='container'>
+	<h2 class='text-info'>FMHApp Survey</h2>
+	<legend class='text-info'>Personal information:</legend>
+	<p class='text-primary'>Identifier:</p>
+	<input type='text' name ='name' value='No Name' required value='1'>
 	<br>
-	Age:<br>
-	<input type="text" name ="age" value="">
+	<p class='text-primary'>Age:</p>
+	<input type='text' name ='age' value=''>
 	<br>
-	</fieldset>
 
-	';
+	";
 	$temp = "";
 	$counter = 1;
     echo "<table>";
@@ -52,42 +52,43 @@ foreach ($conn->query($sql) as $row)
 	if($row[3]!=$temp){
 		$temp = $row[3];
 		echo "<tr><td colspan=2>";
-		echo "<br><br><br>";
-		echo "$temp";
 		echo "<br><br>";
+		echo "<h3 class='text-info'>$temp</h3>";
+		echo "<br>";
 		echo "</td></tr>";
 	}
 	echo "<tr><td>";
-	echo $counter;
-	echo ". ";
+    echo "<p class='text-primary'>$counter. $row[1]</p>";
 
-    echo $row[1];
-
-	 //    echo '
-	 //    	</td><td>
- 	//    	<label class="btn btn-primary">
-	//   		<input type="radio" name="$row[0]" value="Yes" required value="1">Yes</button>
-	//   	</label>
-	//  	<label class="btn btn-primary">
-	//  		<input type="radio" name="$row[0]" value="No" required value="1">No</button>
-	//  	</label>
-	//  	</td></tr>
-	// ';
-    echo "</td><td><input type='radio' name='$row[0]' value='Yes' required value='1'>Yes";
-    echo "    ";
-	echo "<input type='radio' name='$row[0]' value='No' required value='1'>No";
-    echo "</td></tr>";
+    echo "
+    	</td><td>
+    	<label class='btn btn-primary'>
+	  		<input type='radio' name='$row[0]' value='Yes' required value='1'>Yes
+	  	</label>
+	 	<label class='btn btn-primary'>
+	 		<input type='radio' name='$row[0]' value='No' required value='1'>No
+	 	</label>
+	 	</td></tr>
+	";
+ //    echo "</td><td><input type='radio' name='$row[0]' value='Yes' required value='1'>Yes";
+ //    echo "    ";
+	// echo "<input type='radio' name='$row[0]' value='No' required value='1'>No";
+ //    echo "</td></tr>";
     $counter++;
 }
-    echo "</table>";
-	echo'
-	<br><br>
-	<input type="checkbox" name="agreement" value="Agree" required value="1"> I agree to the terms and uses<br><br>
+ 
+	echo"
+		</table>
+		<br><br>
+		<div class='checkbox'>
+		  <label><input type='checkbox' value='Agree'>I agree to the terms and uses</label>
+		</div>
+	 	<label class='btn btn-warning'>
+	 		<input type='submit' name='submit'>Submit
+	 	</label>
 
-	<input type="submit" value="Submit">
-	</form>
-
-	';
+	</div>
+	";
 
 $conn = null;
 
